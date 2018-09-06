fe17ed51bd50dcfaad49efc61cc8838ee5a8ff95
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 04:00:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(){
+function addElementToBeginningOfArray(array, ){
   chocolateBars = ['foo', ...chocolateBars]
   return chocolateBars[1]
 }
