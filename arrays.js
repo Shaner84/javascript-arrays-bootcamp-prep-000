8967f028261b19eeb4194f9eaaab380e3c716346
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 04:07:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 function addElementToEndOfArray(array, element){
   return [...array, element]
 }
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
   return array
 }
