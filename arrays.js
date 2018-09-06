4a3aea1bdd31a437cba353c631adfd1d904becd5
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 04:19:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,7 +16,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
-function destructivelyRemoveElementFromBeginningOfArray(Array){
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
   array.unshift()
   return array
 }
