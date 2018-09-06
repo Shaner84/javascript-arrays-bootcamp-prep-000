546204586d23b8f441bcb3f92fc7604a173ec696
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 03:05:58 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+function addElementToBeginningOfArray(){
+  return ['foo', ...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray(){
+  return chocolateBars.unshift("foo")
+}
