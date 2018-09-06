66d68bb01e6e724cc3d58b6dc062c0113f4beb4f
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 03:18:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-function addElementToBeginningOfArray(){
-  const moreChocolateBars[5] = ["foo", ...chocolateBars]
-    return moreChocolateBars
-}
-function destructivelyAddElementToBeginningOfArray(){
-  
-}
+
 
