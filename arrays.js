667329754ca2d2a940881685b21db8cfcdd8a424
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 04:27:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,5 +21,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
     return array
 }
 function removeElementFromBeginningOfArray(array){
-  array = array.slice(1)
+ array = array.slice(1)
+  return array
 }
