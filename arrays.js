3c68ac5be803082ef072527b67c715c1deb99024
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-09-06 04:31:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,4 +28,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
     return array
 }
-function removeElementFromEndOfArray
+function removeElementFromEndOfArray(array){
+  array = array.slice(0,array.length - 1)
+    return array
+}
